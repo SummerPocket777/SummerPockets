78dd08d531d398fa31e3c3cb39f50169fd620870
罗汉 <1072344372@qq.com>
2024-06-25 18:40:15 +0800

    初始化模块

---
+++ sp-common/sp-common-core/src/main/java/com/sp/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.sp.core.exception;
+
+import com.sp.core.domain.BaseResponse;
+import com.sp.core.domain.ResultUtils;
+import com.sp.core.enums.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+/**
+ * 全局异常处理程序
+ *
+ * @author 罗汉
+ * @date 2023/06/22
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 业务异常处理程序
+     *
+     * @param e e
+     * @return {@link BaseResponse}
+     */
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse businessExceptionHandler(BusinessException e) {
+        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    /**
+     * 运行时异常处理程序
+     *
+     * @param e e
+     * @return {@link BaseResponse}
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse runtimeExceptionHandler(RuntimeException e) {
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+
+}
