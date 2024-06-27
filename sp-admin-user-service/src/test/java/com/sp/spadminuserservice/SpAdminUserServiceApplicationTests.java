5f17d1ec77c9ac2cf726f665064f653f1d5f30a6
HikaruStation <2482213645@qq.com>
2024-06-27 15:06:58 +0800

    创建了管理员用户模块

---
+++ sp-admin-user-service/src/test/java/com/sp/spadminuserservice/SpAdminUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.spadminuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAdminUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
