f47117a4cfc2612a6137da59769c5b56a664a778
HikaruStation <2482213645@qq.com>
2024-06-27 15:31:31 +0800

    修改了管理员用户的pom文件

--- sp-admin-user-service/src/main/java/com/sp/controller/UserController.java
+++ sp-admin-user-service/src/main/java/com/sp/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.sp.controller;
 
+import com.sp.core.common.BaseResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/user")
 public class UserController {
+
+    public BaseResponse<String> login() {
+        return null;
+    }
 }
