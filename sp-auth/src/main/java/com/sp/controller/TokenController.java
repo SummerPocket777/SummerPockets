9f7653aa7496c4f88833fe6a569bb0da812df015
罗汉 <1072344372@qq.com>
2024-06-27 19:11:28 +0800

    权限模块sp-auth模块采用sa-token

---
+++ sp-auth/src/main/java/com/sp/controller/TokenController.java
@@ -0,0 +1,33 @@
+package com.sp.controller;
+
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 令牌控制器
+ *
+ * @author 罗汉
+ * @date 2024/06/27
+ */
+@RestController
+@RequestMapping("/user/")
+public class TokenController {
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(20000);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+
+}
