a9ea8b439e5d0b9e893a87978eb200181e6db59a
ayan <12345679.com>
2024-07-01 12:18:26 +0800

    戴

--- uniapp-consumer-ui/pages/shop/shop.vue
+++ uniapp-consumer-ui/pages/shop/shop.vue
@@ -1,11 +1,59 @@
 <template>
 	<view>
-		点餐页面
-		<view class="a">
-
-			<dishbox class="b"></dishbox>
-			
-			
+		<view class="shop-top">
+			<view class="top-name">
+				<view class="top-img">
+					<u-image width="150rpx" height="150rpx" shape="square" radius="5px"></u-image>
+				</view>
+				<view class="top-text">
+					<view class="text-first">
+						<u-text text="桌名+桌号" bold size="17px"></u-text>
+					</view>
+					<view class="text-second">
+						<u-text text="用餐人数: "></u-text>
+					</view>
+				</view>
+				
+			</view>
+			<view class="top-search">
+				<up-search placeholder="请输入菜品" input-align="center" action-text=""></up-search>
+			</view>
+		</view>
+		<view class="container">
+			<scrolle-view scroll-y="true">
+				<view class="container-left">
+					<view class="menu-left">
+						<view class="all-menu">
+							<text>全 部</text>
+						</view>
+						
+					</view>
+				</view>
+			</scrolle-view>
+			<scroll-view scroll-y="true" >
+				<view class="container-right">
+					<view class="info-right">
+						<view class="image-info">
+							<u-image width="150rpx" height="150rpx" shape="square" radius="5px"></u-image>
+						</view>
+						<view class="detail-info">
+							<view class="info-name">
+							    <u-text text="榴莲面包" bold></u-text>
+							</view>
+							<view class="intro-info">
+								<u-text text="暂无介绍" size="13px"></u-text>
+								<view class="sell-info">
+									<u-text text="销量: " size="13px" ></u-text>
+									<u-text text="0" size="13px"></u-text>
+								</view>
+							</view>
+							<view class="price-info">
+								<u-text prefix-icon="" mode="price" text="29.80" color="red"></u-text>
+							</view>
+						</view>
+					</view>
+				</view>
+			</scroll-view>
 		</view>
 	</view>
 </template>
@@ -21,16 +69,91 @@
 </script>
 
 <style lang="scss">
-	.a{
+.shop-top{
+	width: 100%;
+	height: 300rpx;
+	background: linear-gradient(186deg, #84F9B9 0%,#8fd3f4 100%);
+	border-top: #ffffff solid 1rpx;
+	display: flex;
+	flex-direction: column;
+	// justify-content: center;
+	// align-items: center;
+	.top-name{
+		width: 750rpx;
+		height: 200rpx;
 		display: flex;
-		 flex-wrap: wrap;
-		 height: 1400rpx;
-		
-		width: 100%;
-		.b{
-			margin-top: auto;
-			width: 100%;
-			height: 200rpx;
+		// justify-content: center;
+		align-items: center;
+		.top-img{
+			margin:0 50rpx;
+		}
+		.top-text{
+			margin-top: 40rpx;
+			.text-first{
+				margin-bottom: 10rpx;
+			}
+			.text-second{
+				
+			}
 		}
 	}
+	.top-search{
+		margin-left: 50rpx;
+	}
+	
+}
+.container{
+	width: 100%;
+	height: 900rpx;
+	display: flex;
+	.container-left{
+		background-color: #e6e6e6;
+		width: 200rpx;
+		height: 100%;
+		display: flex;
+		// flex-direction: column;
+		justify-content: center;
+		.all-menu{
+			width: 100rpx;
+			height: 200rpx;
+			text-align: center;
+			margin-top: 50rpx;
+		}
+	}
+	.container-right{
+		width: 550rpx;
+		height: 100%;
+		display: flex;
+		.info-right{
+			width: 100%;
+			height: 25%;
+			display: flex;
+			// flex-direction: column;
+			// justify-content: center;
+			.image-info{
+				width: 200rpx;
+				height: 100%;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+			.detail-info{
+				width: 300rpx;
+				height: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				// align-items: center;
+				.intro-info{
+					.sell-info{
+						display: flex;
+						align-items: center;
+					}
+				}
+				
+			}
+			
+		}
+	}
+}
 </style>
--- uniapp-consumer-ui/pages/user/user.vue
+++ uniapp-consumer-ui/pages/user/user.vue
@@ -1,19 +1,100 @@
 <template>
-	<view>
-		个人中心页面
+	<view class="content">
+		<view class="top">
+			<view class="user-img">
+				
+			</view>
+			<text class="user-name">{{userName}}</text>
+		</view>
+		<view class="container">
+			<view class="text-f">
+				<text class="description">绑定手机号获取更多优惠推荐</text>
+				<view class="right-icon">
+					<up-icon name="arrow-right"></up-icon>
+				</view>
+			</view>
+			<view class="text-s">
+				<text class="description">关于我们</text>
+				<view class="right-icon">
+					<up-icon name="arrow-right"></up-icon>
+				</view>
+			</view>
+			<view class="text-t">
+				<text class="description">我的预约</text>
+				<view class="right-icon">
+					<up-icon name="arrow-right"></up-icon>
+				</view>
+			</view>
+		</view>
 	</view>
+	
+	
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
-			};
+				userName:"用户名称"
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
 		}
 	}
 </script>
 
 <style lang="scss">
+.top{
+	background: linear-gradient(186deg, #84F9B9 0%,#8fd3f4 100%);
+	width: 750rpx;
+	height: 400rpx;
+	display: flex;
+	border-top: #ffffff solid 1px;
+	flex-direction: column;
+	text-align: center;
+	align-items: center;
+	.user-img{
+		width: 200rpx;
+		height: 200rpx;
+		margin-top: 50rpx;
+		border-radius: $uni-border-radius-circle;
+		background: linear-gradient(0deg, #f3e7e9 0%,#e3eeff 99%,#e3eeff 100%);
+	}
+	.user-name{
+		margin-top: 20rpx;
+		color: #00aaff;
+	}
+}
+.container{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	.text-f,.text-s,.text-t{
+			width: 100%;
+			height: 120rpx;
+			background: repeating-linear-gradient(272deg, #5ee7df 0%,#DEB9F4 100%);
+			margin-top: 20rpx;
+			border-radius: 10px;
+			display: flex;
+			align-items: center;
+			.description{
+				// text-align: center;
+				// line-height: 120rpx;
+				margin-left: 50rpx;
+				width: 600rpx;
+			}
+			.right-icon{
+			    // line-height: 120rpx;	
+				margin-left: 20rpx;
+			}
+	}
+	
+	
+}
 
 </style>
