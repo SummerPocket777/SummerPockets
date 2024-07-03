eec739442159c72c2238d40441e6fa70ac71dea7
YOUNGTREEc <2421416467@qq.com>
2024-07-03 19:14:52 +0800

    更新后端优惠券服务

---
+++ sp-voucher/src/main/java/com/sp/controller/VoucherController.java
@@ -0,0 +1,15 @@
+package com.sp.controller;
+
+import com.sp.core.utils.ResponseDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/voucher")
+public class VoucherController {
+
+    @RequestMapping("/test")
+    public ResponseDTO test() {
+        return new ResponseDTO(1,"优惠券获取成功", 123);
+    }
+}
